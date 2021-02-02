bc916b98439f85a84947f0df972c024bc8f63324
Gilberto <gilberto@trackmob.com.br>
2021-02-02 00:53:22 -0300

    feat: add eslint, prettier, typescript, editorconfig, lint-staged and husky

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">React avançado!</a>
         </h1>
 
         <p className={styles.description}>
